6e898a30d79741d9888fe529a7c07aaa3a78b93b
Sinan DAROUKH <s.daroukh@etu.isima.fr>
2019-05-13 09:40:33 +0200

    .

--- src/Migrations/Version20190513073740.php
+++ src/Migrations/Version20190513073740.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20190512181717 extends AbstractMigration
+final class Version20190513073740 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -22,9 +22,11 @@ final class Version20190512181717 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
+        $this->addSql('CREATE TABLE medium (id INT AUTO_INCREMENT NOT NULL, codebarre INT NOT NULL, nom VARCHAR(255) NOT NULL, description VARCHAR(255) NOT NULL, type VARCHAR(255) NOT NULL, localisation VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE search (id INT AUTO_INCREMENT NOT NULL, recherche VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('ALTER TABLE user CHANGE roles roles JSON NOT NULL');
         $this->addSql('ALTER TABLE support CHANGE grammage grammage INT DEFAULT NULL');
-        $this->addSql('ALTER TABLE log ADD updated_at DATETIME NOT NULL');
+        $this->addSql('ALTER TABLE log CHANGE id_product id_product INT DEFAULT NULL');
     }
 
     public function down(Schema $schema) : void
@@ -32,7 +34,9 @@ final class Version20190512181717 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE log DROP updated_at');
+        $this->addSql('DROP TABLE medium');
+        $this->addSql('DROP TABLE search');
+        $this->addSql('ALTER TABLE log CHANGE id_product id_product INT DEFAULT NULL');
         $this->addSql('ALTER TABLE support CHANGE grammage grammage INT DEFAULT NULL');
         $this->addSql('ALTER TABLE user CHANGE roles roles LONGTEXT NOT NULL COLLATE utf8mb4_bin');
     }
